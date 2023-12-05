48184487bb4bac93fba478951be52d907fb4e289
2nthony <ant.hu@outlook.com>
2023-12-05 13:51:57 +0800

    just can run

--- projects/16-open-qf/src/site/postcss.config.js
+++ projects/16-open-qf/src/site/postcss.config.js
@@ -1,6 +1,12 @@
+const path = require("node:path");
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {
+      config: path.join(__dirname, "tailwind.config.js"),
+    },
+
     autoprefixer: {},
   },
-}
+};
--- projects/16-open-qf/src/site/tailwind.config.js
+++ projects/16-open-qf/src/site/tailwind.config.js
@@ -1,18 +1,19 @@
+const { resolve } = require("node:path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    resolve(__dirname, "./pages/**/*.{js,jsx}"),
+    resolve(__dirname, "./components/**/*.{js,jsx}"),
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};

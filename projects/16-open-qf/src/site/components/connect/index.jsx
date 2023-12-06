90b62a1358b1c2c9a7fb3345d3657f662618086b
notset <notset@email.com>
2023-12-06 16:39:20 +0800

    Show connected account

--- projects/16-open-qf/src/site/components/connect/index.jsx
+++ projects/16-open-qf/src/site/components/connect/index.jsx
@@ -1,14 +1,11 @@
 import ConnectWalletButton from "./connectWalletButton";
 import { useSelector } from "react-redux";
 import { accountSelector } from "@/store/reducers/accountSlice";
+import ConnectedAccount from "../user/connectedAccount";
 
 export default function Connect() {
   const account = useSelector(accountSelector);
   console.log(account);
 
-  return (
-    <div>
-      <ConnectWalletButton />
-    </div>
-  );
+  return <div>{account ? <ConnectedAccount /> : <ConnectWalletButton />}</div>;
 }

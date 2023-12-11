ffc20ace33523f83139955ff04252fb9cf79690a
notset <notset@email.com>
2023-12-11 16:07:46 +0800

    fix

--- projects/16-open-qf/src/site/components/tabs/index.jsx
+++ projects/16-open-qf/src/site/components/tabs/index.jsx
@@ -1,8 +1,11 @@
 export default function Tabs({ tabs = [] }) {
   return (
     <div className="flex gap-[40px]">
-      {tabs.map((tab) => (
-        <div className="flex flex-col pb-[16px] border-b-[4px] border-fill-bg-brand-secondary">
+      {tabs.map((tab, index) => (
+        <div
+          key={index}
+          className="flex flex-col pb-[16px] border-b-[4px] border-fill-bg-brand-secondary"
+        >
           {tab.title}
         </div>
       ))}

6f6856c03fd7cc41034cf4eb8569d0f77cbcd35e
2nthony <ant.hu@outlook.com>
2023-12-14 14:52:57 +0800

    init dom

---
+++ projects/16-open-qf/src/site/components/apply/form/donation.jsx
@@ -0,0 +1,18 @@
+import Card from "@/components/card";
+import FormItem from "@/components/form/item";
+import { ChainIcon } from "@osn/common-ui";
+import { Input } from "@osn/common-ui";
+
+export default function ApplyProjectDonationForm() {
+  return (
+    <Card>
+      <FormItem
+        label="Donation Address"
+        description="Please enter the Polkadot mainnet address for receiving funds"
+        htmlFor="donation"
+      >
+        <Input id="donation" suffix={<ChainIcon chainName="polkadot" />} />
+      </FormItem>
+    </Card>
+  );
+}
---
+++ projects/16-open-qf/src/site/components/apply/submit.jsx
@@ -0,0 +1,10 @@
+import { Button } from "../button";
+import Card from "../card";
+
+export default function ApplyProjectSubmitSidebar() {
+  return (
+    <Card>
+      <Button>Submit</Button>
+    </Card>
+  );
+}

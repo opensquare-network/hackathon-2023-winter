f473deae5ba5b239466f97c44b9a1aa89880ffb6
2nthony <ant.hu@outlook.com>
2023-12-14 16:09:07 +0800

    stash info form

--- projects/16-open-qf/src/site/components/apply/form/info.jsx
+++ projects/16-open-qf/src/site/components/apply/form/info.jsx
@@ -1,5 +1,143 @@
 import Card from "@/components/card";
+import FormItem from "@/components/form/item";
+import { cn } from "@/utils";
+import { Input } from "@osn/common-ui";
+import { SystemAdd, SystemDelete } from "@osn/icons/opensquare";
+import { noop } from "lodash-es";
+import { useState } from "react";
+import { useShallowCompareEffect } from "react-use";
+import RichEditor from "@osn/common-ui/es/RichEditor";
 
 export default function ApplyProjectInfoForm() {
-  return <Card>ApplyProjectInfoForm</Card>;
+  const [formValue, setFormValue] = useState({
+    name: "",
+    summary: "",
+    category: "",
+    links: [""],
+    description: "",
+    bannerCid: "",
+    logoCid: "",
+  });
+
+  function updateFormValue(key, value) {
+    setFormValue({ ...formValue, [key]: value });
+  }
+
+  useShallowCompareEffect(() => {
+    console.log(formValue);
+  }, [formValue]);
+
+  return (
+    <Card>
+      <div className="space-y-5">
+        <div className="grid grid-cols-2 gap-5">
+          <FormItem label="Logo"></FormItem>
+          <FormItem label="Cover Image"></FormItem>
+        </div>
+
+        <FormItem htmlFor="name" label="Project Name">
+          <Input
+            id="name"
+            autoComplete="off"
+            value={formValue.name}
+            onChange={(e) => {
+              updateFormValue("name", e.target.value);
+            }}
+          />
+        </FormItem>
+
+        <FormItem
+          htmlFor="summary"
+          label="Summary"
+          description="Displayed in the project card, please tell us about your project briefly"
+        >
+          <Input
+            id="summary"
+            value={formValue.summary}
+            onChange={(e) => {
+              updateFormValue("summary", e.target.value);
+            }}
+          />
+        </FormItem>
+
+        <FormItem
+          label="Category"
+          description="Please select a category for this project to get the right contributors"
+        ></FormItem>
+
+        <FormItem label="Related Links">
+          <RelatedLinksField
+            defaultValue={formValue.links}
+            onChangeValue={(value) => {
+              updateFormValue("links", value);
+            }}
+          />
+        </FormItem>
+
+        <FormItem
+          label="Description"
+          description="Please tell us about your project, aim for 200-500 words"
+        >
+          <RichEditor
+            content={formValue.description}
+            setContent={(value) => {
+              updateFormValue("description", value);
+            }}
+            showButtons={false}
+          />
+        </FormItem>
+      </div>
+    </Card>
+  );
+}
+
+function CategoryTagsField() {}
+
+function RelatedLinksField({ defaultValue = [], onChangeValue = noop }) {
+  const [links, setLinks] = useState(defaultValue);
+
+  useShallowCompareEffect(() => {
+    onChangeValue(links);
+  }, [links]);
+
+  return (
+    <div>
+      <div className="space-y-3">
+        {links.map((value, idx) => (
+          <Input
+            key={idx}
+            value={value}
+            onChange={(e) => {
+              const newLinks = [...links];
+              newLinks[idx] = e.target.value;
+              setLinks(newLinks);
+            }}
+            suffix={
+              idx > 0 && (
+                <SystemDelete
+                  role="button"
+                  className={cn(
+                    "w-5 h-5",
+                    "[&_path]:fill-text-tertiary [&_path]:hover:fill-text-secondary",
+                  )}
+                  onClick={() => {
+                    setLinks(links.filter((_, i) => i !== idx));
+                  }}
+                />
+              )
+            }
+          />
+        ))}
+      </div>
+      <button
+        className="mt-3 flex items-center text14semibold text-text-brand-secondary"
+        onClick={() => {
+          setLinks([...links, ""]);
+        }}
+      >
+        <SystemAdd className="mr-1 w-5 h-5 [&_path]:fill-text-brand-secondary" />
+        New Link
+      </button>
+    </div>
+  );
 }

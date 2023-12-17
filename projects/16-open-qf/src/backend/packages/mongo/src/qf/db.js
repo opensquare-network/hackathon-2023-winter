baa51cf3de54a2245982c24ef6ecde5be4133de9
Yongfeng LI <wliyongfeng@gmail.com>
2023-12-17 19:43:01 +0800

    Save mock tags

--- projects/16-open-qf/src/backend/packages/mongo/src/qf/db.js
+++ projects/16-open-qf/src/backend/packages/mongo/src/qf/db.js
@@ -1,7 +1,7 @@
 const {
-  mongo: { ScanDb },
-  env: { getEnvOrThrow },
+  mongo: { ScanDb }, env: { getEnvOrThrow },
 } = require("@osn/scan-common");
+const { tags } = require("@open-qf/qf-server/src/scripts/mock/tags");
 
 let db = null;
 
@@ -9,18 +9,17 @@ let roundCol = null;
 let projectCol = null;
 let projectCommentCol = null;
 let contributorCol = null;
+let tagCol = null;
 
 async function initQfServerDb() {
-  db = new ScanDb(
-    getEnvOrThrow("MONGO_QF_SERVER_URL"),
-    getEnvOrThrow("MONGO_QF_SERVER_NAME")
-  );
+  db = new ScanDb(getEnvOrThrow("MONGO_QF_SERVER_URL"), getEnvOrThrow("MONGO_QF_SERVER_NAME"));
   await db.init();
 
   roundCol = await db.createCol("round");
   projectCol = await db.createCol("project");
   projectCommentCol = await db.createCol("projectComment");
   contributorCol = await db.createCol("contributor");
+  tagCol = await db.createCol("tag");
   _createIndexes().then(() => console.log("DB indexes created!"));
 }
 
@@ -29,6 +28,8 @@ async function _createIndexes() {
     console.error("Please call initDb first");
     process.exit(1);
   }
+
+  await tagCol.createIndex({ id: 1 }, { unique: true });
 }
 
 async function makeSureInit(col) {
@@ -57,14 +58,15 @@ async function getContributorCol() {
   return contributorCol;
 }
 
+async function getTagCol() {
+  await makeSureInit(tagCol);
+  return tagCol;
+}
+
 function getQfServerDb() {
   return db;
 }
 
 module.exports = {
-  getQfServerDb,
-  getRoundCol,
-  getProjectCol,
-  getProjectCommentCol,
-  getContributorCol,
+  getQfServerDb, getRoundCol, getProjectCol, getProjectCommentCol, getContributorCol, getTagCol,
 };

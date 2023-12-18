f3dc0450e934fa2ab2c530f01297e8a05f834929
Yongfeng LI <wliyongfeng@gmail.com>
2023-12-18 14:24:31 +0800

    Fix raised on round detail page

--- projects/16-open-qf/src/site/components/project/sidebar.jsx
+++ projects/16-open-qf/src/site/components/project/sidebar.jsx
@@ -1,11 +1,11 @@
 import tw from "tailwind-styled-components";
 import { Button } from "@/components/button";
 import { useServerSideProps } from "@/context/serverSideProps";
-import { toPrecision } from "@osn/common";
 import NetworkUser from "../user/networkUser";
 import DonatePopup from "../donatePopup";
 import { useState } from "react";
 import { useAccount } from "@/context/account";
+import LocaleSymbol from "@/components/common/localeSymbol";
 
 const Content = tw.div`flex flex-col gap-[16px]`;
 
@@ -30,16 +30,12 @@ export default function Sidebar() {
   const account = useAccount();
   const [showDonatePopup, setShowDonatePopup] = useState(false);
   const { detail } = useServerSideProps();
-  const totalDot = toPrecision(
-    detail.raised || 0,
-    getDecimalsFromSymbol("DOT"),
-  );
 
   return (
     <div className="w-full p-[32px] shadow-shadow-card-default">
       <div className="flex flex-col gap-[20px] items-center">
         <Content>
-          <Info title="Total Raised" value={`${totalDot} DOT`} />
+          <Info title="Total Raised" value={<LocaleSymbol value={detail.raised || 0}/>} />
           <Info title="Contributors" value={detail.contributorsCount || 0} />
           <Info
             title="Donation Address"

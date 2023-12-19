ce68ebd82e6beb13769559cabcfe82b69b684d56
2nthony <ant.hu@outlook.com>
2023-12-19 16:44:03 +0800

    fix: import

--- projects/16-open-qf/src/site/vitest.config.js
+++ projects/16-open-qf/src/site/vitest.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({

4d3b2ff6c9b0220b225241e7397e50ba78775710
2nthony <ant.hu@outlook.com>
2023-12-19 15:10:58 +0800

    setup react component test

--- projects/16-open-qf/src/site/vitest.config.js
+++ projects/16-open-qf/src/site/vitest.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vitest/config";
 import path from "node:path";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
+    environment: "jsdom",
     alias: {
       "@": path.resolve(__dirname, "."),
     },

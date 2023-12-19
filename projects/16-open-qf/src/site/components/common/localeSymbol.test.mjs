d6b2991f2ae366d3a0ca369bbd9406f526a0cebe
2nthony <ant.hu@outlook.com>
2023-12-19 15:11:07 +0800

    stash

--- projects/16-open-qf/src/site/components/common/localeSymbol.test.mjs
+++ projects/16-open-qf/src/site/components/common/localeSymbol.test.mjs
@@ -1,20 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import LocaleSymbol from "./localeSymbol";
 import { describe, expect, test } from "vitest";
-import LocaleSymbol from "@/components/common/localeSymbol";
 
-describe("localeSymbol output", () => {
+describe("localeSymbol", () => {
   test("base", () => {
-    expect(LocaleSymbol({ value: "1000000000000" })).toBe("100 DOT");
+    render(<LocaleSymbol value="1000000000000" />);
+    expect(screen.getByRole("heading")).toHaveTextContent("100 DOT");
   });
 
-  test("decimal places 0", () => {
+  test.skip("decimal places 0", () => {
     expect(LocaleSymbol({ value: "76819568407885" })).toBe("7,682 DOT");
   });
 
-  test("0", () => {
+  test.skip("0", () => {
     expect(LocaleSymbol({ value: "0" })).toBe("0 DOT");
   });
 
-  test("NaN", () => {
+  test.skip("NaN", () => {
     expect(LocaleSymbol({ value: null })).toBe("NaN DOT");
     expect(LocaleSymbol({ value: undefined })).toBe("NaN DOT");
   });

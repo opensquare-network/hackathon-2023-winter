6b0468c4e8a34e6ee18f065fa54dfb7a1bbe617a
chaojun <chaojun@opensquare.network>
2023-12-20 18:00:36 +0800

    fix: use backdrop

--- projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
+++ projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
@@ -2,35 +2,34 @@ import { Breadcrumb } from "@osn/common-ui";
 import MainContainer from "../containers/main";
 import AppLayout from "./appLayout";
 import { useRouter } from "next/router";
-import { HeaderBg } from "./common";
+import Backdrop from "./backdrop";
 
 export default function ApplyLayout({ children }) {
   const router = useRouter();
 
   return (
     <AppLayout>
-      <HeaderBg className="h-[308px]">
-        <MainContainer className="pt-10 space-y-5">
-          <Breadcrumb
-            backButtonRender={(button) => (
-              <span
-                role="link"
-                onClick={() => {
-                  router.back();
-                }}
-              >
-                {button}
-              </span>
-            )}
-          >
-            <Breadcrumb.Item>
-              <span className="text-text-primary">New Project</span>
-            </Breadcrumb.Item>
-          </Breadcrumb>
+      <Backdrop className="h-[308px]" />
+      <MainContainer className="pt-10 space-y-5">
+        <Breadcrumb
+          backButtonRender={(button) => (
+            <span
+              role="link"
+              onClick={() => {
+                router.back();
+              }}
+            >
+              {button}
+            </span>
+          )}
+        >
+          <Breadcrumb.Item>
+            <span className="text-text-primary">New Project</span>
+          </Breadcrumb.Item>
+        </Breadcrumb>
 
-          <div>{children}</div>
-        </MainContainer>
-      </HeaderBg>
+        <div>{children}</div>
+      </MainContainer>
     </AppLayout>
   );
 }
--- projects/16-open-qf/src/site/components/layouts/detailLayout.jsx
+++ projects/16-open-qf/src/site/components/layouts/detailLayout.jsx
@@ -1,6 +1,6 @@
 import MainContainer from "../containers/main";
 import AppLayout from "./appLayout";
-import { HeaderBg } from "./common";
+import Backdrop from "./backdrop";
 
 export default function DetailLayout({
   children,
@@ -10,15 +10,14 @@ export default function DetailLayout({
 }) {
   return (
     <AppLayout {...props}>
-      <HeaderBg>
-        <MainContainer className="pt-10">
-          <div className="mb-[20px]">{breadcrumb}</div>
-          <div className="flex gap-[20px]">
-            <div className="flex grow">{children}</div>
-            <div className="max-md:hidden min-w-[392px]">{sidebar}</div>
-          </div>
-        </MainContainer>
-      </HeaderBg>
+      <Backdrop className="h-[240px]" />
+      <MainContainer className="pt-10">
+        <div className="mb-[20px]">{breadcrumb}</div>
+        <div className="flex gap-[20px]">
+          <div className="flex grow">{children}</div>
+          <div className="max-md:hidden min-w-[392px]">{sidebar}</div>
+        </div>
+      </MainContainer>
     </AppLayout>
   );
 }

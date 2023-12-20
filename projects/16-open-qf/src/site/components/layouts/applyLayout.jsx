7240d553b1be2466486604c7c587be6df478052e
chaojun <chaojun@opensquare.network>
2023-12-20 17:32:16 +0800

    fix: apply page header bg

--- projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
+++ projects/16-open-qf/src/site/components/layouts/applyLayout.jsx
@@ -2,32 +2,35 @@ import { Breadcrumb } from "@osn/common-ui";
 import MainContainer from "../containers/main";
 import AppLayout from "./appLayout";
 import { useRouter } from "next/router";
+import { HeaderBg } from "./common";
 
 export default function ApplyLayout({ children }) {
   const router = useRouter();
 
   return (
     <AppLayout>
-      <MainContainer className="pt-10 space-y-5">
-        <Breadcrumb
-          backButtonRender={(button) => (
-            <span
-              role="link"
-              onClick={() => {
-                router.back();
-              }}
-            >
-              {button}
-            </span>
-          )}
-        >
-          <Breadcrumb.Item>
-            <span className="text-text-primary">New Project</span>
-          </Breadcrumb.Item>
-        </Breadcrumb>
+      <HeaderBg className="h-[308px]">
+        <MainContainer className="pt-10 space-y-5">
+          <Breadcrumb
+            backButtonRender={(button) => (
+              <span
+                role="link"
+                onClick={() => {
+                  router.back();
+                }}
+              >
+                {button}
+              </span>
+            )}
+          >
+            <Breadcrumb.Item>
+              <span className="text-text-primary">New Project</span>
+            </Breadcrumb.Item>
+          </Breadcrumb>
 
-        <div>{children}</div>
-      </MainContainer>
+          <div>{children}</div>
+        </MainContainer>
+      </HeaderBg>
     </AppLayout>
   );
 }
